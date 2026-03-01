adfac811141321138dcb1174c7f9d3c6e0eec37d
KaiChuul <kyriakos.savvides@gmail.com>
2026-03-01 16:18:13 +0200

    Update variables.tf

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -9,3 +9,9 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type for the dev server"
+  type        = string
+  default     = "t3.micro"
+}
